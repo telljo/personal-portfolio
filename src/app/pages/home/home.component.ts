56af18faf42a75bc06b8a9b20116d7aa06b9a479
Josh Tell <jtell1997@gmail.com>
2026-04-15 11:35:21 +0800

    Implement new design system

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['home.component.scss'],
   host: { class: 'home' }
 })
 export class HomeComponent {
-  constructor() { }
+  readonly currentYear = new Date().getFullYear();
 }
